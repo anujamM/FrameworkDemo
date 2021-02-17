23ec7f83f611f1158901f797436bf188df8d6f9c
Mondal <AM051163@Cerner.net>
2021-02-17 07:14:21 -0500

    Init Commit

---
+++ src/main/java/FrameworkDemo/FrameworkDemo/App.java
@@ -0,0 +1,13 @@
+package FrameworkDemo.FrameworkDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
